f2542b28955c0b42a654c6410da43ccf09e143c5
Leandro Correia da Costa <correialesp@gmail.com>
2020-01-17 15:58:11 -0300

    ajustments in the folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <React.Fragment>
-            <Backdrop show={props.show} closed={props.closed} />
-            <div 
-                className={classes.Modal}
-                style = {{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                { props.children }
-            </div>
-        </React.Fragment>
-    )
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} closed={this.props.closed} />
+                <div 
+                    className={classes.Modal}
+                    style = {{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    { this.props.children }
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
-export default modal;
+export default Modal;
